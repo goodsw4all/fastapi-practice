6beacabc92dfe15ff5d10d20880974b4812e474e
Myoungwoo Jang <codeart@gmail.com>
2022-07-07 20:14:06 +0900

    03 GET method router path query path+query 'http://127.0.0.1:8000/blog/1/comments/1?valid=false&username=test'

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
+from typing import Optional
 from fastapi import FastAPI
+from enum import Enum
 
 app = FastAPI()
 
 
 @app.get('/hello')
-def index():
+def index():  # Name doesn't matter, but not good for code reading
     return {'message': 'Hello, world'}
 
 
+@app.get('/blog/all')
+def index(page=1, page_size: Optional[int] = None):
+    return {'message': f'All {page_size} blogs on page {page}'}
+
+
+@app.get('/blog/{id}/comments/{comment_id}')
+def index(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
+
+
+class BlogType(str, Enum):
+    short = 'short'
+    story = 'story'
+    howto = 'howto'
+
+
+@app.get('/blog/type/{type}')
+def index(type: BlogType):
+    return {'message': f'{type}'}
+
+
+@app.get('/blog/{id}')
+def index(id: int):  # pydantic for type validation
+    return {'message': f'{id}'}
