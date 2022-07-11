74ac2707eea2f8cc7519c3355396964ad7252335
Myoungwoo Jang <codeart@gmail.com>
2022-07-12 08:51:23 +0900

    04 Operation description - Response status code - Documentation (http://127.0.0.1:8000/docs)

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status
 from enum import Enum
 
 app = FastAPI()
@@ -10,13 +10,23 @@ def index():  # Name doesn't matter, but not good for code reading
     return {'message': 'Hello, world'}
 
 
-@app.get('/blog/all')
+@app.get('/blog/all',
+         tags=['blog'],
+         summary='Retrieve all blogs',
+         description='This api is fetching all blogs',
+         response_description='list of blogs'
+         )
 def index(page=1, page_size: Optional[int] = None):
     return {'message': f'All {page_size} blogs on page {page}'}
 
 
-@app.get('/blog/{id}/comments/{comment_id}')
-def index(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+@app.get('/blog/{id}/comments/{comment_id}', tags=['blog', 'comment'])
+def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    """
+    Simulates retriving a comment of a blog
+    - **id** : mandatory argument
+    """
+
     return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
 
 
@@ -26,11 +36,14 @@ class BlogType(str, Enum):
     howto = 'howto'
 
 
-@app.get('/blog/type/{type}')
+@app.get('/blog/type/{type}', tags=['blog'])
 def index(type: BlogType):
     return {'message': f'{type}'}
 
 
-@app.get('/blog/{id}')
-def index(id: int):  # pydantic for type validation
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, tags=['blog'])
+def index(id: int, response: Response):  # pydantic for type validation
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'err': 'not found'}
     return {'message': f'{id}'}
