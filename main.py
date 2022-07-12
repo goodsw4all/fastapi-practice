6f2fb9be6cad6f9e1028ddcb5d78ced67a1fe78b
Myoungwoo Jang <codeart@gmail.com>
2022-07-12 18:32:03 +0900

    05 APIRouters - Categorized way to making APIs

--- main.py
+++ main.py
@@ -1,49 +1,13 @@
-from typing import Optional
 from fastapi import FastAPI, Response, status
-from enum import Enum
+
+from router import blog_get
+from router import blog_post
 
 app = FastAPI()
+app.include_router(blog_get.router)  # Modulization
+app.include_router(blog_post.router)
 
 
 @app.get('/hello')
 def index():  # Name doesn't matter, but not good for code reading
     return {'message': 'Hello, world'}
-
-
-@app.get('/blog/all',
-         tags=['blog'],
-         summary='Retrieve all blogs',
-         description='This api is fetching all blogs',
-         response_description='list of blogs'
-         )
-def index(page=1, page_size: Optional[int] = None):
-    return {'message': f'All {page_size} blogs on page {page}'}
-
-
-@app.get('/blog/{id}/comments/{comment_id}', tags=['blog', 'comment'])
-def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
-    """
-    Simulates retriving a comment of a blog
-    - **id** : mandatory argument
-    """
-
-    return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
-
-
-class BlogType(str, Enum):
-    short = 'short'
-    story = 'story'
-    howto = 'howto'
-
-
-@app.get('/blog/type/{type}', tags=['blog'])
-def index(type: BlogType):
-    return {'message': f'{type}'}
-
-
-@app.get('/blog/{id}', status_code=status.HTTP_200_OK, tags=['blog'])
-def index(id: int, response: Response):  # pydantic for type validation
-    if id > 5:
-        response.status_code = status.HTTP_404_NOT_FOUND
-        return {'err': 'not found'}
-    return {'message': f'{id}'}
---
+++ router/blog_get.py
@@ -0,0 +1,47 @@
+from typing import Optional
+from fastapi import FastAPI, Response, status, APIRouter
+from enum import Enum
+
+
+router = APIRouter(
+    prefix='/blog',
+    tags=['blog']
+)
+
+
+@router.get('/all',
+            summary='Retrieve all blogs',
+            description='This api is fetching all blogs',
+            response_description='list of blogs'
+            )
+def index(page=1, page_size: Optional[int] = None):
+    return {'message': f'All {page_size} blogs on page {page}'}
+
+
+@router.get('/blog/{id}/comments/{comment_id}', tags=['comment'])
+def get_comment(id: int, comment_id: int, valid: bool = True, username: Optional[str] = None):
+    """
+    Simulates retriving a comment of a blog
+    - **id** : mandatory argument
+    """
+
+    return {'message': f'blog_id {id}, comment_id {comment_id}, valid {valid}, username {username}'}
+
+
+class BlogType(str, Enum):
+    short = 'short'
+    story = 'story'
+    howto = 'howto'
+
+
+@router.get('/type/{type}')
+def index(type: BlogType):
+    return {'message': f'{type}'}
+
+
+@router.get('/{id}', status_code=status.HTTP_200_OK)
+def index(id: int, response: Response):  # pydantic for type validation
+    if id > 5:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {'err': 'not found'}
+    return {'message': f'{id}'}
