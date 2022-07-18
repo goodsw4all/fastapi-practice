db5da4afb7ff3ecd977b25af45d17e7614c1c32b
Myoungwoo Jang <codeart@gmail.com>
2022-07-18 11:38:57 +0900

    07 : SqlAlchemy

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from fastapi import FastAPI, Response, status
-
+from typing import Optional
+from fastapi import FastAPI
 from router import blog_get
 from router import blog_post
+from router import user
+from router import article
+from db.database import engine
+from db import models
+
 
 app = FastAPI()
-app.include_router(blog_get.router)  # Modulization
+app.include_router(user.router)
+app.include_router(article.router)
+app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 
-
 @app.get('/hello')
-def index():  # Name doesn't matter, but not good for code reading
-    return {'message': 'Hello, world'}
+def index():
+  return {'message': 'Hello world!'}
+
+models.Base.metadata.create_all(engine)
