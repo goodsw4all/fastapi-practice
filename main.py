e1e200406c8a41c27fed6b847dbc24d0201492f4
Myoungwoo Jang <codeart@gmail.com>
2022-07-27 09:10:08 +0900

    section 8-22

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-from typing import Optional
-from fastapi import FastAPI
+from pydoc import plainpager
+from urllib.request import Request
+from exceptions import StoryException
 from router import blog_get
 from router import blog_post
 from router import user
 from router import article
 from db.database import engine
 from db import models
+from fastapi import Request, status
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import JSONResponse, PlainTextResponse
 
 
 app = FastAPI()
@@ -14,8 +18,23 @@ app.include_router(article.router)
 app.include_router(blog_get.router)
 app.include_router(blog_post.router)
 
+
 @app.get('/hello')
 def index():
-  return {'message': 'Hello world!'}
+    return {'message': 'Hello world!'}
+
+
+@app.exception_handler(StoryException)
+def story_exception_handler(request: Request, exc: StoryException):
+    return JSONResponse(
+        status_code=status.HTTP_418_IM_A_TEAPOT,
+        content={'detail': exc.name}
+    )
+
+
+# @app.exception_handler(HTTPException)
+# def story_exception_handler(request: Request, exc: HTTPException):
+#     return PlainTextResponse(str(exc), status_code=400)
+
 
 models.Base.metadata.create_all(engine)
--- router/article.py
+++ router/article.py
@@ -6,18 +6,22 @@ from db.database import get_db
 from db import db_article
 
 router = APIRouter(
-  prefix='/article',
-  tags=['article']
+    prefix='/article',
+    tags=['article']
 )
 
 # Create article
+
+
 @router.post('/', response_model=ArticleDisplay)
 def create_article(request: ArticleBase, db: Session = Depends(get_db)):
-  return db_article.create_article(db, request)
+    return db_article.create_article(db, request)
 
 # Get specific article
-@router.get('/{id}') #, response_model=ArticleDisplay)
+
+
+@router.get('/{id}')  # , response_model=ArticleDisplay)
 def get_article(id: int, db: Session = Depends(get_db)):
-  return {
-    'data': db_article.get_article(db, id)
-  }
+    return {
+        'data': db_article.get_article(db, id)
+    }
